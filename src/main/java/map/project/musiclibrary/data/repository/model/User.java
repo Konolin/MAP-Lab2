1bac105ede9fef1f217fe003ffafb002d66d5c18
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-10-27 12:44:27 +0300

    User
    basic implementation

---
+++ src/main/java/map/project/musiclibrary/data/repository/model/User.java
@@ -0,0 +1,30 @@
+package map.project.musiclibrary.data.repository.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "birthDate")
+    private Date birthDate;
+}
