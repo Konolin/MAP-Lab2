3f33a6abaa6c6833596007d9292bea0b610592ad
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-10-30 13:26:59 +0200

    Advert Service

---
+++ src/main/java/map/project/musiclibrary/data/repository/AdvertisementRepository.java
@@ -0,0 +1,6 @@
+package map.project.musiclibrary.data.repository;
+
+import map.project.musiclibrary.data.repository.model.Advertisement;
+
+public interface AdvertisementRepository extends BaseRepository<Advertisement> {
+}
