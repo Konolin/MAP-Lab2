ad66ed0f5083dcd50491b652b777d442e2b9c312
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-10-30 10:53:39 +0200

    Advertisement2

--- src/main/java/map/project/musiclibrary/data/repository/model/Advertisement.java
+++ src/main/java/map/project/musiclibrary/data/repository/model/Advertisement.java
@@ -1,2 +1,20 @@
-package map.project.musiclibrary.data.repository.model;public class Advertisment {
+package map.project.musiclibrary.data.repository.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "advertisments")
+public class Advertisement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "company")
+    private String company;
+
+    @Column(name = "type")
+    private AdvertisementType advertisementType;
 }
--- src/main/java/map/project/musiclibrary/data/repository/model/AdvertisementType.java
+++ src/main/java/map/project/musiclibrary/data/repository/model/AdvertisementType.java
@@ -1,2 +1,8 @@
-package map.project.musiclibrary.data.repository.model;public class AdvertismentType {
+package map.project.musiclibrary.data.repository.model;
+
+public enum AdvertisementType {
+    INSURANCE,
+    FAST_FOOD,
+    MEDICINE,
+    GAMBLING
 }
