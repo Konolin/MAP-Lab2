c2f03437888a39119f14d4ccdc43e99031c7a4b3
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-10-30 09:40:14 +0200

    UserService created

--- src/main/java/map/project/musiclibrary/data/repository/UserRepository.java
+++ src/main/java/map/project/musiclibrary/data/repository/UserRepository.java
@@ -3,5 +3,8 @@ package map.project.musiclibrary.data.repository;
 import map.project.musiclibrary.data.repository.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByName(String name);
 }
