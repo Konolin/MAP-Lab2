b0cdc6691b8c980c938eaacb7e64fa6d1c78ddba
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-10-31 11:03:49 +0200

    Fixed missing @Data

--- src/main/java/map/project/musiclibrary/data/repository/model/Song.java
+++ src/main/java/map/project/musiclibrary/data/repository/model/Song.java
@@ -1,9 +1,13 @@
 package map.project.musiclibrary.data.repository.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "songs")
+@Data
 public class Song extends Audio {
     @Column(name = "genre")
     private String genre;
