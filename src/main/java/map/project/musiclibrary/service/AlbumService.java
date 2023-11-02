6cd18b720e1f9294a321ae4b6786441798c99046
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-11-02 09:29:18 +0200

    AlbumService

---
+++ src/main/java/map/project/musiclibrary/service/AlbumService.java
@@ -0,0 +1,30 @@
+package map.project.musiclibrary.service;
+
+import map.project.musiclibrary.data.repository.AlbumRepository;
+import map.project.musiclibrary.data.repository.model.Album;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlbumService {
+    final AlbumRepository albumRepository;
+
+    @Autowired
+    public AlbumService(AlbumRepository albumRepository) {
+        this.albumRepository = albumRepository;
+    }
+
+    public Album save(Album album) {
+        return albumRepository.save(album);
+    }
+
+    public Album findByName(String name) {
+        return albumRepository.findByName(name).stream().findFirst().orElse(null);
+    }
+
+    public List<Album> findAll() {
+        return albumRepository.findAll();
+    }
+}
---
+++ src/main/java/map/project/musiclibrary/service/ArtistService.java
@@ -0,0 +1,4 @@
+package map.project.musiclibrary.service;
+
+public class ArtistService {
+}
