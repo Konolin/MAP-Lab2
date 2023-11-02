df90bc8f787be5617b4844b81cacec2fb4a4de33
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-11-02 09:36:04 +0200

    PlaylistService

--- src/main/java/map/project/musiclibrary/service/PlaylistService.java
+++ src/main/java/map/project/musiclibrary/service/PlaylistService.java
@@ -1,4 +1,30 @@
 package map.project.musiclibrary.service;
 
+import map.project.musiclibrary.data.repository.PlaylistRepository;
+import map.project.musiclibrary.data.repository.model.Playlist;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PlaylistService {
+    private final PlaylistRepository playlistRepository;
+
+    @Autowired
+    public PlaylistService(PlaylistRepository playlistRepository) {
+        this.playlistRepository = playlistRepository;
+    }
+
+    public Playlist save(Playlist playlist) {
+        return playlistRepository.save(playlist);
+    }
+
+    public Playlist findByName(String name) {
+        return playlistRepository.findByName(name).stream().findFirst().orElse(null);
+    }
+
+    public List<Playlist> findAll() {
+        return playlistRepository.findAll();
+    }
 }
