a467bf252aff3e357b20929bd0a31229508f1e68
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-11-02 08:44:39 +0200

    Host service and repo fix

--- src/main/java/map/project/musiclibrary/data/repository/PodcastRepository.java
+++ src/main/java/map/project/musiclibrary/data/repository/PodcastRepository.java
@@ -1,7 +1,11 @@
 package map.project.musiclibrary.data.repository;
 
+import map.project.musiclibrary.data.repository.model.Advertisement;
 import map.project.musiclibrary.data.repository.model.Podcast;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PodcastRepository extends JpaRepository<Podcast, Long> {
+    List<Podcast> findByName(String name);
 }
