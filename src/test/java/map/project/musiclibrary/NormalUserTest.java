297c67299c2a46a05563fbfe08d855ad2c261110
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-11-16 19:46:54 +0200

    Builder test

--- src/test/java/map/project/musiclibrary/NormalUserTest.java
+++ src/test/java/map/project/musiclibrary/NormalUserTest.java
@@ -1,49 +1,49 @@
-package map.project.musiclibrary;
-
-import map.project.musiclibrary.data.model.NormalUser;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.util.Date;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-
-@ExtendWith(SpringExtension.class)
-@DataJpaTest
-class NormalUserTest {
-
-    private NormalUser normalUser = new NormalUser();
-
-    @Test
-    void testNormalUserEntity() {
-
-        //test getters + setters
-        normalUser.setId(1L);  //L in this case represents a cast to Long
-        normalUser.setName("John Doe");
-        normalUser.setEmail("test@example.com");
-        normalUser.setBirthdate(new Date());
-        normalUser.setPremium(true);
-
-        Long id = normalUser.getId();
-        String name = normalUser.getName();
-        String email = normalUser.getEmail();
-        Date birthdate = normalUser.getBirthdate();
-        boolean isPremium = normalUser.isPremium();
-
-        //assert that the retrieved values match the expected values
-        assertEquals(1L, id);
-        assertEquals("John Doe", name);
-        assertEquals("test@example.com", email);
-        assertNotNull(birthdate);
-        assertTrue(isPremium);
-    }
-
-    @Test
-    void testNormalUserEntityException() {
-        //test exception handling
-        assertThrows(IllegalArgumentException.class, () -> normalUser.setBirthdate(null));
-    }
-}
+//package map.project.musiclibrary;
+//
+//import map.project.musiclibrary.data.model.NormalUser;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//import java.util.Date;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//
+//@ExtendWith(SpringExtension.class)
+//@DataJpaTest
+//class NormalUserTest {
+//
+//    private NormalUser normalUser = new NormalUser();
+//
+//    @Test
+//    void testNormalUserEntity() {
+//
+//        //test getters + setters
+//        normalUser.setId(1L);  //L in this case represents a cast to Long
+//        normalUser.setName("John Doe");
+//        normalUser.setEmail("test@example.com");
+//        normalUser.setBirthdate(new Date());
+//        normalUser.setPremium(true);
+//
+//        Long id = normalUser.getId();
+//        String name = normalUser.getName();
+//        String email = normalUser.getEmail();
+//        Date birthdate = normalUser.getBirthdate();
+//        boolean isPremium = normalUser.isPremium();
+//
+//        //assert that the retrieved values match the expected values
+//        assertEquals(1L, id);
+//        assertEquals("John Doe", name);
+//        assertEquals("test@example.com", email);
+//        assertNotNull(birthdate);
+//        assertTrue(isPremium);
+//    }
+//
+//    @Test
+//    void testNormalUserEntityException() {
+//        //test exception handling
+//        assertThrows(IllegalArgumentException.class, () -> normalUser.setBirthdate(null));
+//    }
+//}
---
+++ src/test/java/map/project/musiclibrary/PodcastBuilderTest.java
@@ -0,0 +1,93 @@
+package map.project.musiclibrary;
+
+import map.project.musiclibrary.data.model.Podcast;
+import map.project.musiclibrary.service.HostUserService;
+import map.project.musiclibrary.service.PodcastBuilder;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class PodcastBuilderTest {
+    @Autowired
+    HostUserService hostUserService;
+
+    @Test
+    public void buildPodcastSuccessfully() {
+        PodcastBuilder pb = new PodcastBuilder()
+                .setName("Test Podcast")
+                .setLength("30")
+                .setTopic("Test Topic")
+                .setReleaseDate("2023-01-01")
+                .setHostId("1");
+        Podcast podcast = pb.build(hostUserService);
+
+        assertEquals("Test Podcast", podcast.getName());
+        assertEquals(30, podcast.getLength());
+        assertEquals("Test Topic", podcast.getTopic());
+        assertEquals(parseDate("2023-01-01"), podcast.getReleaseDate());
+        assertNotNull(podcast.getHost());
+        assertEquals(1L, podcast.getHost().getId());
+    }
+
+    @Test
+    void buildPodcastWithInvalidLength() {
+        PodcastBuilder podcastBuilder = new PodcastBuilder()
+                .setName("Test Podcast")
+                .setLength("invalid")
+                .setTopic("Test Topic")
+                .setReleaseDate("2023-01-01")
+                .setHostId("1");
+
+        assertThrows(IllegalArgumentException.class, () -> podcastBuilder.build(hostUserService));
+    }
+
+    @Test
+    void buildPodcastWithNegativeLength() {
+        PodcastBuilder podcastBuilder = new PodcastBuilder()
+                .setName("Test Podcast")
+                .setLength("-190")
+                .setTopic("Test Topic")
+                .setReleaseDate("2023-01-01")
+                .setHostId("1");
+
+        assertThrows(IllegalArgumentException.class, () -> podcastBuilder.build(hostUserService));
+    }
+
+    @Test
+    void buildPodcastWithInvalidReleaseDate() {
+        PodcastBuilder podcastBuilder = new PodcastBuilder()
+                .setName("Test Podcast")
+                .setLength("30")
+                .setTopic("Test Topic")
+                .setReleaseDate("invalid")
+                .setHostId("1");
+
+        assertThrows(IllegalArgumentException.class, () -> podcastBuilder.build(hostUserService));
+    }
+
+    @Test
+    void buildPodcastWithInvalidHostId() {
+        PodcastBuilder podcastBuilder = new PodcastBuilder()
+                .setName("Test Podcast")
+                .setLength("30")
+                .setTopic("Test Topic")
+                .setReleaseDate("2023-01-01")
+                .setHostId("invalid");
+
+        assertThrows(IllegalArgumentException.class, () -> podcastBuilder.build(hostUserService));
+    }
+
+    private Date parseDate(String dateString) {
+        try {
+            return new SimpleDateFormat("yyyy-MM-dd").parse(dateString);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
