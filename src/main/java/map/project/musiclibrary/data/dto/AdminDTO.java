4c43c5fcf3a959d1a8aaf717fde706d67a6fe5d4
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-12-13 19:45:59 +0200

    Admin rest

---
+++ src/main/java/map/project/musiclibrary/data/dto/AdminDTO.java
@@ -0,0 +1,9 @@
+package map.project.musiclibrary.data.dto;
+
+import lombok.Data;
+
+@Data
+public class AdminDTO {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/map/project/musiclibrary/data/dto/LoginResponseDTO.java
@@ -0,0 +1,9 @@
+package map.project.musiclibrary.data.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginResponseDTO {
+    private String status;
+    private String message;
+}
