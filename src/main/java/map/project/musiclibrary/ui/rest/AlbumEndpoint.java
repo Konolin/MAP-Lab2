5f36a8d87e27c2cf54411e933428ec5f43d51c16
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-12-13 20:07:49 +0200

    Album rest

--- src/main/java/map/project/musiclibrary/ui/rest/AlbumEndpoint.java
+++ src/main/java/map/project/musiclibrary/ui/rest/AlbumEndpoint.java
@@ -1,4 +1,63 @@
 package map.project.musiclibrary.ui.rest;
 
+import map.project.musiclibrary.data.dto.AlbumDTO;
+import map.project.musiclibrary.data.model.users.UserSession;
+import map.project.musiclibrary.service.AlbumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/album")
 public class AlbumEndpoint {
+    private final AlbumService albumService;
+    private final UserSession userSession;
+
+    @Autowired
+    public AlbumEndpoint(AlbumService albumService, UserSession userSession) {
+        this.albumService = albumService;
+        this.userSession = userSession;
+    }
+
+    @PostMapping("/list")
+    public String listAlbums() {
+        if (userSession.isLoggedIn()) {
+            return albumService.findAll().toString();
+        } else {
+            return "Only logged in users can list all albums";
+        }
+    }
+
+    @PostMapping("/add")
+    public String addAlbum(@RequestBody AlbumDTO request) {
+        if (userSession.isLoggedIn()) {
+            try {
+                return albumService.addAlbum(request.getName(), request.getArtistId(), request.getSongIds()).toString();
+            } catch (NumberFormatException e) {
+                return "Error: Invalid integer format. Please provide valid numbers for song IDs.";
+            } catch (IllegalArgumentException e) {
+                return e.getMessage();
+            }
+        } else {
+            return "Only logged in users can add an album";
+        }
+    }
+
+    @PostMapping("/find")
+    public String findAlbum(@RequestParam String name) {
+        return albumService.findByName(name).toString();
+    }
+
+    @PostMapping("/delete")
+    public String deleteAlbum(@RequestParam String idStr) {
+        if (userSession.isLoggedIn()) {
+            try {
+                albumService.delete(idStr);
+                return "Album with ID " + idStr + " has been deleted successfully!";
+            } catch (NumberFormatException e) {
+                return "Error: Invalid integer format. Please provide a valid number.";
+            }
+        } else {
+            return "Only logged in users can delete albums.";
+        }
+    }
 }
